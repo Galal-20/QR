0bb2c6c407549c1f4b16b151e691adb9ba989089
Galal-20 <ga71387@gmail.com>
2023-05-27 16:41:10 +0300

    Save QR code

--- app/src/main/java/com/example/qrgenerator/MainActivity.kt
+++ app/src/main/java/com/example/qrgenerator/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.qrgenerator
 
+import android.content.ContentValues
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.WriterException
 import com.google.zxing.qrcode.QRCodeWriter
@@ -21,6 +25,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var buttonGenerator:Button
 
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -46,10 +51,42 @@ class MainActivity : AppCompatActivity() {
                         }
                     }
                     idIvQrcode.setImageBitmap(bmp)
+
+                    saveQRCodeToGallery(bmp)
+
                 }catch (_:WriterException){
 
                 }
             }
+        }
+    }
+
+    private fun saveQRCodeToGallery(qrCodeBitmap: Bitmap) {
+        val filename = "QRCode_${System.currentTimeMillis()}.png"
+
+        val contentValues = ContentValues().apply {
+            put(MediaStore.Images.Media.DISPLAY_NAME, filename)
+            put(MediaStore.Images.Media.MIME_TYPE, "image/png")
+        }
+
+        val resolver = contentResolver
+        val imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+
+        if (imageUri != null) {
+            resolver.openOutputStream(imageUri).use { outputStream ->
+                qrCodeBitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+            }
+
+            // Notify the gallery app to scan for new media
+            sendBroadcast(Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, imageUri))
+
+            // Optional: Show a toast or provide feedback to the user
+            Toast.makeText(this, "QR code saved to gallery", Toast.LENGTH_SHORT).show()
+        } else {
+            // Failed to create the image file
+            // Handle the scenario accordingly
+            Toast.makeText(this, "Check your permissions", Toast.LENGTH_SHORT).show()
+
         }
     }
 
